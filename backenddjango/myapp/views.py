0d07a99dbbfd12e7704aea795f0b804fd4f63f7b
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2023-09-02 16:31:25 +0530

    tableadded

--- backenddjango/myapp/views.py
+++ backenddjango/myapp/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+peoples = [
+    {'name':'dhruv','age':22},
+    {'name':'divyam','age':22},
+    {'name':'ayush','age':21},
+    {'name':'abhinav','age':17}
+]
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context = {'peoples': peoples})
 
 def about(request):
     return render(request, "about.html")
