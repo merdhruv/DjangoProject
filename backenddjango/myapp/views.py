61b4f18c1565ee82a54c5f68d258b8f7927411ad
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2023-09-09 09:31:56 +0530

    truncate

--- backenddjango/myapp/views.py
+++ backenddjango/myapp/views.py
@@ -10,8 +10,12 @@ peoples = [
     {'name':'abhinav','age':17}
 ]
 
+text = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+
 def index(request):
-    return render(request, "index.html", context = {'peoples': peoples})
+    return render(request, "index.html", context = {'peoples': peoples, 'text': text})
 
 def about(request):
     return render(request, "about.html")
