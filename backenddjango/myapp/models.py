80831d6dfc1c1388a0c2c74ad2621b9591e4fc58
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2023-09-11 23:17:30 +0530

    911

--- backenddjango/myapp/models.py
+++ backenddjango/myapp/models.py
@@ -5,7 +5,7 @@ class Student(models.Model):
     name = models.CharField(max_length = 100)
     age = models.IntegerField()
     email = models.EmailField()
-    address = models.TextField()
+    address = models.TextField(null=True, blank=True)
     File = models.FileField()
     
 
