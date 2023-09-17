8720fadbb5def2df233048eb196110c3f3a74c98
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2023-09-17 09:44:02 +0530

    carsadded

--- backenddjango/myapp/models.py
+++ backenddjango/myapp/models.py
@@ -8,5 +8,9 @@ class Student(models.Model):
     address = models.TextField(null=True, blank=True)
     
     
-class Product(models.Model):
-    pass
+class Car(models.Model):
+    car_name = models.CharField(max_length = 100)
+    speed = models.IntegerField(default = 50)
+
+    def __str__(self) ->str:
+        return self.car_name
